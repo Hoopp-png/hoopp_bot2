86cfd281a1b82dee4a1635e88f2c8096c9e6a456
Hoopp <73751966+Hoopp-png@users.noreply.github.com>
2026-06-27 02:02:09 -0600

    Replace !userinfo command with !perfil and add new commands

--- hoopp-bot-1/commands/comandos.js
+++ hoopp-bot-1/commands/comandos.js
@@ -10,7 +10,12 @@ module.exports = {
       .setDescription('Aquí están los comandos que puedes usar:')
       .addFields(
         { name: '!ping', value: '`Pong`' },
-        { name: '!userinfo', value: '`Muestra información de un usuario de discord y genshin.`' },
+        { name: '!perfil', value: '`Muestra información de un usuario de discord y genshin.`' },
+         { name: '!daily', value: '`¡Gana 100 HooppCoins diarias!`' },
+         { name: '!donar', value: '`no lo he hecho.`' },
+         { name: '!apostar', value: '`Apuesta tilin, pierde todas tus hooppscoins.`' },
+         { name: '!top', value: '`Muestra información del top 10 de la quincena.`' },
+         { name: '!cosas', value: '`Muestra información de tu inventario en Hoopp Web.`' },
        
     )
       .setFooter({ text: 'Desarrollado por Hoopp', iconURL: message.author.displayAvatarURL() })
